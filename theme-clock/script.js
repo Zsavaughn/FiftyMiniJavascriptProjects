01d78a7c2b3864b5383a1c4c8113448819934cbf
Zsavaughn <37762426+Zsavaughn@users.noreply.github.com>
2021-02-24 00:15:38 -0500

    fixed problem of hour 12 showing as 0

--- theme-clock/script.js
+++ theme-clock/script.js
@@ -37,7 +37,7 @@ function setTime() {
 
     secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
 
-    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+    timeEl.innerHTML = `${hoursForClock === 0 ? "12" : hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
 
     dateEl.innerHTML = `${days[day]}, ${months[month]} <span class ="circle">${date}</span>`
 }
